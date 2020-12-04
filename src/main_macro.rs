182cf3aad9918826ea9df5e1e6d47e1a0feef09d
Giacomo Stevanato <giaco.stevanato@gmail.com>
2020-12-04 21:09:52 +0100

    Simplified main_macro

--- src/main_macro.rs
+++ src/main_macro.rs
@@ -6,12 +6,41 @@ macro_rules! main {
         $( mod $d; )*
 
         fn main() {
-            const DEFAULT_DAY: Option<&str> = $crate::main!(@LATEST_DAY $( $d )*);
+			let days = [$( stringify!($d) )*];
+			let default_day = days.last().copied();
 
-            aoc_helper::internal::run_clap($year, DEFAULT_DAY.map(|s| &s[3..]), |requested_day| {
+            aoc_helper::internal::run_clap($year, default_day.map(|s| &s[3..]), |requested_day| {
                 let mut total = ::std::time::Duration::default();
                 let mut found = false;
-                $crate::main!(@MAIN $year requested_day total found $( $d )*);
+				
+				$(
+					if requested_day == Some(&stringify!($d)[3..]) || requested_day == Some("all") {
+						found = true;
+						
+						const DAY: &str = stringify!($d);
+						println!("Day {:<2}", &DAY[3..]);
+						let input = aoc_helper::internal::get_input($year, &DAY[3..]);
+					
+						let now = std::time::Instant::now();
+						let input = $d::input_generator(input.trim_end());
+						let elapsed = now.elapsed();
+						total += elapsed;
+						println!("     - Parsing input");
+						println!("       Took {:.3?}", elapsed);
+						println!();
+					
+						let now = std::time::Instant::now();
+						let part1_solution = $d::part1(&input);
+						let elapsed = now.elapsed();
+						total += elapsed;
+						println!("     - Part 1: {}", part1_solution);
+						println!("       Took {:.3?}", elapsed);
+						println!();
+					
+						$crate::main!(@PART2 input total $d);
+					}
+				)*
+				
                 if found {
                     println!("Took in total: {:.3?}", total);
                 } else {
@@ -20,38 +49,6 @@ macro_rules! main {
                 println!();
             });
         }
-    };
-    (@LATEST_DAY) => { None };
-    (@LATEST_DAY $d:ident) => { Some(stringify!($d)) };
-    (@LATEST_DAY $d:ident $($o:ident)+) => { $crate::main!(@LATEST_DAY $( $o )+) };
-    (@MAIN $year:literal $requested_day:ident $total:ident $found:ident $($d:ident)*) => {
-        $(
-            if $requested_day == Some(&stringify!($d)[3..]) || $requested_day == Some("all") {
-                $found = true;
-                
-                const DAY: &str = stringify!($d);
-                println!("Day {:<2}", &DAY[3..]);
-                let input = aoc_helper::internal::get_input($year, &DAY[3..]);
-            
-                let now = std::time::Instant::now();
-                let input = $d::input_generator(input.trim_end());
-                let elapsed = now.elapsed();
-                $total += elapsed;
-                println!("     - Parsing input");
-                println!("       Took {:.3?}", elapsed);
-                println!();
-            
-                let now = std::time::Instant::now();
-                let part1_solution = $d::part1(&input);
-                let elapsed = now.elapsed();
-                $total += elapsed;
-                println!("     - Part 1: {}", part1_solution);
-                println!("       Took {:.3?}", elapsed);
-                println!();
-            
-                $crate::main!(@PART2 input $total $d);
-            }
-        )*
     };
     (@PART2 $input:ident $total:ident day25) => {};
     (@PART2 $input:ident $total:ident $d:ident) => {
