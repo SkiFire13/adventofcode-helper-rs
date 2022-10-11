e61fa86751794587bdc32c58d853c23584a430b6
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-10-11 17:15:52 +0200

    Update template

--- template.rs
+++ template.rs
@@ -1,13 +1,13 @@
 #[allow(unused_imports)]
 use super::prelude::*;
-type Input = ;
+type Input = ();
 
 pub fn input_generator(input: &str) -> Input {
-    
+
 }
 
-pub fn part1(input: &Input) ->  {
-
+pub fn part1(input: &Input) -> usize {
+    0
 }
 
 pub fn part2(input: &Input) -> usize {
